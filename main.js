bc6b37f7d34c48bbdea45b7b571434551a710a05
Youngho <dudgh6237@naver.com>
2023-04-23 11:04:23 +0900

    DO LIST에 항목 추가 기능 + CSS 보완

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//1. 입력창 시간 출력하기
+//======= 1. 입력창 시간 출력하기 ==========================
 let headerClock = document.getElementById("headerClock");
 
 function clock() {
@@ -25,4 +25,37 @@ function clock() {
 }
 clock();
 setInterval(clock,1000);
+
+//======= 2.DO LIST 추가하기 ===============================
+function addList() {
+  let addList = document.getElementById("addInput");
+  //구현  
+  if(addList.value == "") {
+    alert("할 일을 작성해주세요 :)");
+  } else {
+  //입력값 받기
+    let label = document.createElement("label");
+  label.setAttribute("id", "ckeck_btn");
+  label.textContent = addList.value;
+  //체크박스 만들기
+  let circle = document.createElement("i");
+  circle.classList.add("circle");
+  label.prepend(circle);
+
+  let ckb = document.createElement("input");
+  ckb.setAttribute("type", "checkbox");
+  ckb.setAttribute("id", "ckeck_btn");
+  label.prepend(ckb);
+  //버튼 만들기
+  let completBtn = document.createElement("button");
+  completBtn.textContent = "완료";
+  label.appendChild(completBtn);
+  //do list로 이동
+  let doList = document.getElementById("doList");
+  doList.appendChild(label);
+  }
+  //리셋
+  addList.value = "";
+  addList.focus();
+}
 
